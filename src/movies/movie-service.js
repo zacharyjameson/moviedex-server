df56936d49a914cbd0eb3084c8ea32f3bab15363
Zack Jameson <zacharymjameson@gmail.com>
2021-04-28 10:15:35 -0400

    movie-router and migrations creation

---
+++ src/movies/movie-service.js
@@ -0,0 +1,25 @@
+const MovieService = {
+  getAllMovies(knex) {
+    return knex.select("*").from("moviedex_movies");
+  },
+
+  insertMovie(knex, newMovie) {
+    return knex
+      .insert(newMovie)
+      .into("moviedex_movies")
+      .returning("*")
+      .then((rows) => {
+        return rows[0];
+      });
+  },
+
+  getById(knex, id) {
+      return knex.from("moviedex_movies").select("*").where("id", id).first();
+  },
+
+  deleteMovie(knex, id) {
+      return knex("moviedex_movies").where({ id }).delete();
+  },
+
+  clearAllMovies(knex,)
+};
